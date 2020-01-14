51ae84677c542ad78853d97d4168b1518cbee89b
Emin <shikhnebiev01@mail.ru>
2020-01-14 12:47:47 +0300

    Новый файл

--- p_bot.py
+++ p_bot.py
@@ -1,5 +1,11 @@
 from telegram.ext import Updater, CommandHandler
+import logging
 
+
+logging.basicConfig(format = '%(name)s - %(levelname)s - %(message)s - %(asctime)s',
+                    level = logging.INFO,
+                    filename = 'bot.log'
+                    )
 
 def start(bot, update):
     text = 'Вызван /start'
@@ -9,7 +15,9 @@ def start(bot, update):
     
 def main():
     mybot = Updater("566724368:AAFbx-AjF0vPNrNCPitG2FDUNN6F436cjE0", use_context=True)
-
+    
+    logging.info('Бот запускается')
+    
     mybot.dispatcher.add_handler(CommandHandler('start',start))
     
     mybot.start_polling()
